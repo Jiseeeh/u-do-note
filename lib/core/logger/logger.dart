10c67bae0ff3d3da70c6c108344dc0c1c1679c4b
Jiseeeh <johncarlo.camara1@gmail.com>
2024-02-05 17:57:31 +0800

    feat: add logger

---
+++ lib/core/logger/logger.dart
@@ -0,0 +1,32 @@
+import 'package:logger/logger.dart';
+
+var logger = Logger(
+  printer: PrettyPrinter(
+      methodCount: 2, // Number of method calls to be displayed
+      errorMethodCount: 8, // Number of method calls if stacktrace is provided
+      lineLength: 120, // Width of the output
+      colors: true, // Colorful log messages
+      printEmojis: true, // Print an emoji for each log message
+      printTime: false // Should each log print contain a timestamp
+      ),
+);
+
+void logWarningOnly() {
+  Logger.level = Level.warning;
+}
+
+/* 
+Samples 
+
+logger.t("Trace log");
+
+logger.d("Debug log");
+
+logger.i("Info log");
+
+logger.w("Warning log");
+
+logger.e("Error log", error: 'Test Error');
+
+logger.f("What a fatal log", error: error, stackTrace: stackTrace);
+ */

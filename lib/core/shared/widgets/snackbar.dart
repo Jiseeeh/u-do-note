8000db3d68fc4c4705bbbffda823fcef288cb33d
Jiseeeh <johncarlo.camara1@gmail.com>
2024-02-17 23:54:36 +0800

    docs: add todo

--- lib/core/shared/widgets/snackbar.dart
+++ lib/core/shared/widgets/snackbar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 // TODO: can be improved or just use a package for snackbars
+// TODO: move this folder `widgets` to `shared/presentation/`
 
 SnackBar createSnackbar(String message) {
   return SnackBar(

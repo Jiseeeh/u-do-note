4e9566b5a3fce47f66f658302cbaa44777bd1fb2
Jiseeeh <johncarlo.camara1@gmail.com>
2024-02-21 00:40:18 +0800

    feat: add shared firebase auth provider

---
+++ lib/core/shared/presentation/providers/shared_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'shared_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$firebaseAuthHash() => r'7791bf70ce0f01bf991a53a76abc915478673c0b';
+
+/// See also [firebaseAuth].
+@ProviderFor(firebaseAuth)
+final firebaseAuthProvider = AutoDisposeProvider<FirebaseAuth>.internal(
+  firebaseAuth,
+  name: r'firebaseAuthProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$firebaseAuthHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FirebaseAuthRef = AutoDisposeProviderRef<FirebaseAuth>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member

3af1a46699f3cac0f20fec4fdb4a62b0d5099f83
Jiseeeh <johncarlo.camara1@gmail.com>
2024-02-22 23:24:52 +0800

    chore: remove unused import

--- lib/features/review_page/presentation/pages/review_screen.dart
+++ lib/features/review_page/presentation/pages/review_screen.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:u_do_note/core/shared/presentation/providers/shared_provider.dart';

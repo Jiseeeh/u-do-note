1d9b4b123d86e59c96fe2a6142f07b1ac11a989c
Jiseeeh <johncarlo.camara1@gmail.com>
2024-02-29 02:45:23 +0800

    feat: add temp note taking screen

---
+++ lib/features/note_taking/presentation/pages/note_taking_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:auto_route/auto_route.dart';
+
+@RoutePage()
+class NoteTakingScreen extends ConsumerStatefulWidget {
+  const NoteTakingScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _NoteTakingScreenState();
+}
+
+class _NoteTakingScreenState extends ConsumerState<NoteTakingScreen> {
+  final _controller = QuillController.basic();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Note Taking'),
+      ),
+      body: Column(
+        children: [
+          QuillToolbar.simple(
+            configurations: QuillSimpleToolbarConfigurations(
+                controller: _controller, multiRowsDisplay: false),
+          ),
+          Expanded(
+            child: QuillEditor.basic(
+              configurations: QuillEditorConfigurations(
+                padding: const EdgeInsets.all(8),
+                controller: _controller,
+                readOnly: false,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}

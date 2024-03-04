bd0c434559f6b310ccf01fddd009c0415f1bdb15
Jiseeeh <johncarlo.camara1@gmail.com>
2024-03-04 23:47:16 +0800

    feat: make parameters required

--- lib/features/note_taking/domain/repositories/note_repository.dart
+++ lib/features/note_taking/domain/repositories/note_repository.dart
@@ -1,9 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:u_do_note/core/error/failures.dart';
 import 'package:u_do_note/core/shared/data/models/note.dart';
+import 'package:u_do_note/features/note_taking/data/models/notebook.dart';
 
 abstract class NoteRepository {
-  Future<Either<Failure, void>> createNote(NoteModel note);
-  Future<Either<Failure, void>> updateNote(String id);
-  Future<Either<Failure, void>> deleteNote(String id);
+  Future<Either<Failure, void>> createNote({required NoteModel note});
+  Future<Either<Failure, void>> updateNote({required String id});
+  Future<Either<Failure, void>> deleteNote({required String id});
+  Future<Either<Failure, List<NotebookModel>>> getNotebooks();
+  Future<Either<Failure, String>> createNotebook({required String name});
 }

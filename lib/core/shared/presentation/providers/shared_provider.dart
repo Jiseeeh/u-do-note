3802a0f53cc84f48f091fdf272ce1fa6ff924cda
Jiseeeh <johncarlo.camara1@gmail.com>
2024-03-04 23:46:26 +0800

    feat: add firestore to shared providers

--- lib/core/shared/presentation/providers/shared_provider.dart
+++ lib/core/shared/presentation/providers/shared_provider.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -6,4 +7,9 @@ part 'shared_provider.g.dart';
 @riverpod
 FirebaseAuth firebaseAuth(FirebaseAuthRef ref) {
   return FirebaseAuth.instance;
+}
+
+@riverpod
+FirebaseFirestore firestore(FirestoreRef ref) {
+  return FirebaseFirestore.instance;
 }
--- lib/core/shared/presentation/providers/shared_provider.g.dart
+++ lib/core/shared/presentation/providers/shared_provider.g.dart
@@ -20,5 +20,19 @@ final firebaseAuthProvider = AutoDisposeProvider<FirebaseAuth>.internal(
 );
 
 typedef FirebaseAuthRef = AutoDisposeProviderRef<FirebaseAuth>;
+String _$firestoreHash() => r'ef4a6b0737caace50a6d79dd3e4e2aa1bc3031d5';
+
+/// See also [firestore].
+@ProviderFor(firestore)
+final firestoreProvider = AutoDisposeProvider<FirebaseFirestore>.internal(
+  firestore,
+  name: r'firestoreProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$firestoreHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FirestoreRef = AutoDisposeProviderRef<FirebaseFirestore>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member

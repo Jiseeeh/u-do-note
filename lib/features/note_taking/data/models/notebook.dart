ea3c01f87ba264c0c400bea1be06cf56c390c33e
Jiseeeh <johncarlo.camara1@gmail.com>
2024-03-04 23:48:54 +0800

    feat: add notebook model

---
+++ lib/features/note_taking/data/models/notebook.dart
@@ -0,0 +1,53 @@
+import 'package:u_do_note/core/shared/data/models/note.dart';
+import 'package:u_do_note/features/note_taking/domain/entities/notebook.dart';
+
+class NotebookModel {
+  final String id;
+  final String subject;
+  final DateTime createdAt;
+  final List<NoteModel> notes;
+
+  NotebookModel({
+    required this.id,
+    required this.subject,
+    required this.createdAt,
+    required this.notes,
+  });
+
+  /// from entity to model
+  factory NotebookModel.fromEntity(NotebookEntity entity) {
+    return NotebookModel(
+      id: entity.id,
+      subject: entity.subject,
+      createdAt: entity.createdAt,
+      notes: entity.notes.map((e) => NoteModel.fromEntity(e)).toList(),
+    );
+  }
+
+  /// from model to entity
+  NotebookEntity toEntity() {
+    return NotebookEntity(
+      id: id,
+      subject: subject,
+      createdAt: createdAt,
+      notes: notes.map((e) => e.toEntity()).toList(),
+    );
+  }
+
+  /// from firestore to model
+  factory NotebookModel.fromFirestore(String id, Map<String, dynamic> data) {
+    return NotebookModel(
+      id: id,
+      subject: data['subject'],
+      createdAt: DateTime.parse(data['created_at'].toDate().toString()),
+      notes: (data['notes'] as List)
+          .map((e) => NoteModel.fromFirestore(e))
+          .toList(),
+    );
+  }
+
+  @override
+  String toString() {
+    return 'NotebookModel(id: $id, subject: $subject, createdAt: $createdAt, notes: ${notes.map((e) => e.title).toList()})';
+  }
+}

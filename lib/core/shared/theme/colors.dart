dedcdf211abbbc1438dafc6007f251e51d8570fb
Jiseeeh <johncarlo.camara1@gmail.com>
2024-03-04 23:46:37 +0800

    feat: add global colors

---
+++ lib/core/shared/theme/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const lightBlue = Color(0xffeaf2ff);
+  static const darkBlue = Color(0xff006ffd);
+  static const jetBlack = Color(0xff343434);
+  static const grey = Color(0xff71727A);
+}

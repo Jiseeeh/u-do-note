598f61e67e5eebd9ba1da80e31f102559e414833
Jiseeeh <johncarlo.camara1@gmail.com>
2024-03-11 01:27:16 +0800

    feat: match return types from data source

--- lib/features/note_taking/domain/repositories/note_repository.dart
+++ lib/features/note_taking/domain/repositories/note_repository.dart
@@ -4,9 +4,12 @@ import 'package:u_do_note/core/shared/data/models/note.dart';
 import 'package:u_do_note/features/note_taking/data/models/notebook.dart';
 
 abstract class NoteRepository {
-  Future<Either<Failure, void>> createNote({required NoteModel note});
-  Future<Either<Failure, void>> updateNote({required String id});
-  Future<Either<Failure, void>> deleteNote({required String id});
+  Future<Either<Failure, String>> createNote(
+      {required String notebookId, required String title});
+  Future<Either<Failure, bool>> updateNote(
+      {required String notebookId, required NoteModel note});
+  Future<Either<Failure, String>> deleteNote(
+      {required String notebookId, required String noteId});
   Future<Either<Failure, List<NotebookModel>>> getNotebooks();
   Future<Either<Failure, String>> createNotebook({required String name});
 }

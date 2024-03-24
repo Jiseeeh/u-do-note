357e2c76082309c53144b2a85fa98db9d2524d89
Jiseeeh <johncarlo.camara1@gmail.com>
2024-03-24 17:35:48 +0800

    feat: add leitner data source for usecases

--- lib/features/review_page/data/datasources/leitner_remote_datasource.dart
+++ lib/features/review_page/data/datasources/leitner_remote_datasource.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dart_openai/dart_openai.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:u_do_note/core/shared/data/models/note.dart';
+
+import 'package:u_do_note/core/firestore_collection_enum.dart';
+import 'package:u_do_note/core/logger/logger.dart';
 import 'package:u_do_note/features/review_page/data/models/leitner.dart';
 
 class LeitnerRemoteDataSource {
@@ -11,11 +13,8 @@ class LeitnerRemoteDataSource {
 
   LeitnerRemoteDataSource(this._firestore);
 
-  Future<List<FlashcardModel>> generateFlashcards(
-      String userId, String userNoteId) async {
-    // fetch the notes in firestore
-    List<NoteModel> notes = await _getNotes(_firestore);
-
+  Future<LeitnerSystemModel> generateFlashcards(
+      String userNotebookId, String content) async {
     // feed it to the openai api to get the flashcards
     final systemMessage = OpenAIChatCompletionChoiceMessageModel(
         role: OpenAIChatMessageRole.system,
@@ -34,9 +33,8 @@ class LeitnerRemoteDataSource {
         ]);
 
     // get notes contents
-    String contents = notes.map((note) => note.content).join(' ');
     String prompt =
-        "Create five(5) flashcards using these notes of mine: '$contents'";
+        "Create five(5) flashcards using these notes of mine. Take note that this is a rich text content, Here it is: '$content'";
 
     final userMessage = OpenAIChatCompletionChoiceMessageModel(
         role: OpenAIChatMessageRole.user,
@@ -62,40 +60,127 @@ class LeitnerRemoteDataSource {
       maxTokens: 600,
     );
 
-    String? content = chatCompletion.choices.first.message.content!.first.text;
+    String? completionContent =
+        chatCompletion.choices.first.message.content!.first.text;
 
-    print('content: $content');
-    print('finish reason: ${chatCompletion.choices.first.finishReason}');
-    print(chatCompletion.systemFingerprint);
-    print(chatCompletion.usage.promptTokens);
+    logger.i('content: $completionContent');
+    logger.i('finish reason: ${chatCompletion.choices.first.finishReason}');
+    logger.i(chatCompletion.systemFingerprint);
+    logger.i(chatCompletion.usage.promptTokens);
 
-    var decodedJson = json.decode(content!);
+    var decodedJson = json.decode(completionContent!);
 
     List<FlashcardModel> flashcards = [];
 
     for (var flashcard in decodedJson['flashcards']) {
       flashcards.add(FlashcardModel.fromJson(flashcard));
     }
+    var userId = FirebaseAuth.instance.currentUser!.uid;
 
-    return flashcards;
+    // Save the flashcards to firestore to be updated
+    // after the user has reviewed the flashcards.
+    var doc = await _firestore
+        .collection(FirestoreCollection.users.name)
+        .doc(userId)
+        .collection(FirestoreCollection.user_notes.name)
+        .doc(userNotebookId)
+        .collection(FirestoreCollection.remarks.name)
+        .add(<String, dynamic>{
+      'review_method': LeitnerSystemModel.name,
+      'flashcards': flashcards.map((flashcard) => flashcard.toJson()).toList(),
+      'score': '',
+      'remark': '',
+    });
+
+    var leitnerSystemModel = LeitnerSystemModel(
+      id: doc.id,
+      userNotebookId: userNotebookId,
+      flashcards: flashcards,
+    );
+
+    return leitnerSystemModel;
   }
 
-  Future<List<NoteModel>> _getNotes(FirebaseFirestore firestore) async {
-    var user = FirebaseAuth.instance.currentUser;
-    List<NoteModel> notesModel = [];
+  Future<String> analyzeFlashcardsResult(
+      String notebookId, LeitnerSystemModel leitnerSystemModel) async {
+    List<int> responseTimes = leitnerSystemModel.flashcards
+        .map((flashcard) => flashcard.elapsedSecBeforeAnswer)
+        .toList();
 
-    var notes = await firestore
-        .collection('users')
-        .doc(user!.uid)
-        .collection('user_notes')
-        .get();
+    final systemMessage = OpenAIChatCompletionChoiceMessageModel(
+        role: OpenAIChatMessageRole.system,
+        content: [
+          OpenAIChatCompletionChoiceMessageContentItemModel.text(
+            "You are a helpful assistant that wants to help students to analyze the results of their review session using the Leitner System.",
+          ),
+        ]);
 
-    for (var note in notes.docs) {
-      for (Map<String, dynamic> firestoreNote in note.data()['notes']) {
-        notesModel.add(NoteModel.fromFirestore(firestoreNote));
-      }
-    }
+    final assistantMessage = OpenAIChatCompletionChoiceMessageModel(
+        role: OpenAIChatMessageRole.assistant,
+        content: [
+          OpenAIChatCompletionChoiceMessageContentItemModel.text(
+            "Return the result as JSON with the properties score, and remark. Note that the score can be from 0 to 10 and the remark can be 'Excellent', 'Good', 'Average', 'Poor', or 'Very Poor'.",
+          ),
+        ]);
 
-    return notesModel;
+    // get notes contents
+    String prompt =
+        "Given the response times of the user, '$responseTimes', give me the score and remark of the user's review session.";
+
+    final userMessage = OpenAIChatCompletionChoiceMessageModel(
+        role: OpenAIChatMessageRole.user,
+        content: [
+          OpenAIChatCompletionChoiceMessageContentItemModel.text(
+            prompt,
+          ),
+        ]);
+
+    final requestMessages = [
+      systemMessage,
+      assistantMessage,
+      userMessage,
+    ];
+
+    OpenAIChatCompletionModel chatCompletion =
+        await OpenAI.instance.chat.create(
+      model: "gpt-3.5-turbo-0125",
+      responseFormat: {"type": "json_object"},
+      // seed: 6,
+      messages: requestMessages,
+      temperature: 0.2,
+      maxTokens: 600,
+    );
+
+    String? completionContent =
+        chatCompletion.choices.first.message.content!.first.text;
+
+    logger.i('content: $completionContent');
+    logger.i('finish reason: ${chatCompletion.choices.first.finishReason}');
+    logger.i(chatCompletion.systemFingerprint);
+    logger.i(chatCompletion.usage.promptTokens);
+
+    var decodedJson = json.decode(completionContent!);
+    var userId = FirebaseAuth.instance.currentUser!.uid;
+
+    logger.i("Saving remarks to firestore...");
+
+    await _firestore
+        .collection(FirestoreCollection.users.name)
+        .doc(userId)
+        .collection(FirestoreCollection.user_notes.name)
+        .doc(notebookId)
+        .collection(FirestoreCollection.remarks.name)
+        .doc(leitnerSystemModel.id)
+        .update({
+      'flashcards': leitnerSystemModel.flashcards
+          .map((flashcard) => flashcard.toJson())
+          .toList(),
+      'score': decodedJson['score'],
+      'remark': decodedJson['remark'],
+    });
+
+    logger.i("Remarks saved to firestore.");
+
+    return "Your remark is ${decodedJson['remark']} with a score of ${decodedJson['score']}.";
   }
 }

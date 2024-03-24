ec8e363b0cdb21959c3944db0611036b8a4c8231
Jiseeeh <johncarlo.camara1@gmail.com>
2024-03-24 17:40:25 +0800

    feat: add generate flashcards use case

--- lib/features/review_page/domain/usecases/generate_flashcards.dart
+++ lib/features/review_page/domain/usecases/generate_flashcards.dart
@@ -1,16 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:u_do_note/core/error/failures.dart';
 import 'package:u_do_note/features/review_page/data/models/leitner.dart';
-import 'package:u_do_note/features/review_page/domain/repositories/leitner_sytem_repository.dart';
+import 'package:u_do_note/features/review_page/domain/repositories/leitner_system_repository.dart';
 
 class GenerateFlashcards {
   final LeitnerSystemRepository _leitnerSystemRepository;
 
   GenerateFlashcards(this._leitnerSystemRepository);
 
-  Future<Either<Failure, List<FlashcardModel>>> call(
-      {required String userId, required String userNoteId}) {
-    return _leitnerSystemRepository.generateFlashcards(
-        userId: userId, userNoteId: userNoteId);
+  Future<Either<Failure, LeitnerSystemModel>> call(
+      String userNotebookId, String content) async {
+    return await _leitnerSystemRepository.generateFlashcards(
+        userNotebookId, content);
   }
 }

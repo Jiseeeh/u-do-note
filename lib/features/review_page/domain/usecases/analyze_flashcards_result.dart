dfc14c7c2017764290d91ae7def91feebf942f51
Jiseeeh <johncarlo.camara1@gmail.com>
2024-03-24 17:40:10 +0800

    feat: add analyze results use case

---
+++ lib/features/review_page/domain/usecases/analyze_flashcards_result.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:u_do_note/core/error/failures.dart';
+import 'package:u_do_note/features/review_page/data/models/leitner.dart';
+import 'package:u_do_note/features/review_page/domain/repositories/leitner_system_repository.dart';
+
+class AnalyzeFlashcardsResult {
+  final LeitnerSystemRepository _leitnerSystemRepository;
+
+  AnalyzeFlashcardsResult(this._leitnerSystemRepository);
+
+  Future<Either<Failure, String>> call(
+      String userNotebookId, LeitnerSystemModel leitnerSystemModel) async {
+    return await _leitnerSystemRepository.analyzeFlashcardsResult(
+        userNotebookId, leitnerSystemModel);
+  }
+}

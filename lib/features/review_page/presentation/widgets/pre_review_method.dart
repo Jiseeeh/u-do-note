fe8cff21ba7db0bf689cb3509e80ed1f27649790
Jiseeeh <johncarlo.camara1@gmail.com>
2024-03-25 23:47:16 +0800

    feat!: add option to check use old flashcards

--- lib/features/review_page/presentation/widgets/pre_review_method.dart
+++ lib/features/review_page/presentation/widgets/pre_review_method.dart
@@ -21,7 +21,12 @@ class PreReviewMethod extends ConsumerStatefulWidget {
 
 class _PreReviewMethodState extends ConsumerState<PreReviewMethod> {
   var notebookId = "";
+  var oldFlashcardId = "";
   List<String> pages = [];
+  var formKey = GlobalKey<FormState>();
+  var titleController = TextEditingController();
+  final minTitleName = 3;
+  final maxTitleName = 18;
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +39,6 @@ class _PreReviewMethodState extends ConsumerState<PreReviewMethod> {
     };
   }
 
-  // TODO: test with a user without notebooks
   AlertDialog _buildDialog(
       BuildContext context, List<NotebookEntity> notebooks) {
     if (notebooks.isEmpty) {
@@ -71,6 +75,10 @@ class _PreReviewMethodState extends ConsumerState<PreReviewMethod> {
         ),
         TextButton(
           onPressed: () async {
+            if (!formKey.currentState!.validate()) {
+              return;
+            }
+
             if (notebookId.isEmpty || pages.isEmpty) {
               EasyLoading.showError("Please select a notebook and pages.");
               return;
@@ -89,13 +97,134 @@ class _PreReviewMethodState extends ConsumerState<PreReviewMethod> {
             switch (widget.reviewMethod) {
               case ReviewMethods.leitnerSystem:
                 EasyLoading.show(
+                    status: 'Checking if you have old flashcards to review...',
+                    maskType: EasyLoadingMaskType.black,
+                    dismissOnTap: false);
+
+                var oldLeitnerModels = await ref
+                    .read(leitnerSystemProvider.notifier)
+                    .getOldFlashcards(notebookId);
+
+                EasyLoading.dismiss();
+
+                //? when the user has old flashcards
+                if (oldLeitnerModels.isNotEmpty && context.mounted) {
+                  var reviewOld = await showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                            title: const Text('Notice'),
+                            scrollable: true,
+                            content: const Text(
+                                'You have old flashcards to review. Do you want to review them first?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(false);
+                                },
+                                child: const Text('No'),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(true);
+                                },
+                                child: const Text('Yes'),
+                              ),
+                            ],
+                          ));
+
+                  //? when the user wants to review the old flashcards
+                  if (reviewOld && context.mounted) {
+                    var selectItems = oldLeitnerModels
+                        .map((leitnerModel) => MultiSelectItem(
+                            leitnerModel.id, leitnerModel.title))
+                        .toList();
+
+                    await showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                              scrollable: true,
+                              title: const Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text("Flashcards to review"),
+                                  Text(
+                                    "The last selected notebook will be used.",
+                                    style: TextStyle(fontSize: 12),
+                                  )
+                                ],
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('Cancel'),
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    context.router.push(LeitnerSystemRoute(
+                                        notebookId: notebookId,
+                                        leitnerSystemModel: oldLeitnerModels
+                                            .firstWhere((leitnerModel) =>
+                                                leitnerModel.id ==
+                                                oldFlashcardId)));
+                                  },
+                                  child: const Text('Continue'),
+                                ),
+                              ],
+                              content: MultiSelectDialogField(
+                                listType: MultiSelectListType.CHIP,
+                                items: selectItems,
+                                decoration: BoxDecoration(
+                                  color: Colors.blue.withOpacity(0.1),
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(8)),
+                                ),
+                                onSelectionChanged: (values) {
+                                  if (values.isEmpty) {
+                                    oldFlashcardId = "";
+                                  }
+
+                                  if (values.length > 1) {
+                                    // remove the first one
+                                    values.removeAt(0);
+                                  }
+                                },
+                                onConfirm: (results) {
+                                  setState(() {
+                                    oldFlashcardId = results.first!;
+                                  });
+                                },
+                                buttonIcon: const Icon(
+                                  Icons.arrow_drop_down_circle_outlined,
+                                  color: Colors.blue,
+                                ),
+                                buttonText: const Text(
+                                  "Notebooks",
+                                ),
+                              ),
+                            ));
+
+                    if (context.mounted) {
+                      Navigator.pop(context);
+                    }
+                    return;
+                  }
+                }
+
+                if (!context.mounted) {
+                  return;
+                }
+
+                EasyLoading.show(
                     status: 'Generating Flashcards...',
                     maskType: EasyLoadingMaskType.black,
                     dismissOnTap: false);
 
                 var failureOrLeitner = await ref
                     .read(leitnerSystemProvider.notifier)
-                    .generateFlashcards(notebookId, contentFromPages);
+                    .generateFlashcards(
+                        titleController.text, notebookId, contentFromPages);
 
                 EasyLoading.dismiss();
 
@@ -108,7 +237,6 @@ class _PreReviewMethodState extends ConsumerState<PreReviewMethod> {
                         leitnerSystemModel: leitnerSystem));
                   }
                 });
-
                 break;
               case ReviewMethods.feynmanTechnique:
                 break;
@@ -123,111 +251,137 @@ class _PreReviewMethodState extends ConsumerState<PreReviewMethod> {
           child: const Text('Continue'),
         ),
       ],
-      content: Column(
-        children: [
-          // Chip(label: Text(widget.reviewMethod.toString())),
-          MultiSelectDialogField(
-            title: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Notebooks"),
-                Text(
-                  "The last selected notebook will be used.",
-                  style: TextStyle(fontSize: 12),
-                )
-              ],
+      content: Form(
+        key: formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              controller: titleController,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Please enter a title.";
+                }
+
+                if (value.length < minTitleName) {
+                  return "Title must be at least $minTitleName characters.";
+                }
+
+                if (value.length > maxTitleName) {
+                  return "Title must be at most $maxTitleName characters.";
+                }
+
+                return null;
+              },
+              decoration: const InputDecoration(
+                labelText: "Title",
+                hintText: "Enter a title for your flashcards",
+                border: InputBorder.none,
+              ),
             ),
+            const SizedBox(height: 10),
+            MultiSelectDialogField(
+              title: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Notebooks"),
+                  Text(
+                    "The last selected notebook will be used.",
+                    style: TextStyle(fontSize: 12),
+                  )
+                ],
+              ),
 
-            items: notebooks
-                .map((notebook) =>
-                    MultiSelectItem(notebook.id, notebook.subject))
-                .toList(),
-            // selectedColor: Colors.blue,
-            listType: MultiSelectListType.CHIP,
-            decoration: BoxDecoration(
-              color: Colors.blue.withOpacity(0.1),
-              borderRadius: const BorderRadius.all(Radius.circular(8)),
-            ),
-            onSelectionChanged: (values) {
-              if (values.isEmpty) {
-                setState(() {
-                  notebookId = "";
-                });
-              }
-
-              if (values.length > 1) {
-                // remove the first one
-                values.removeAt(0);
-              }
-            },
-            onConfirm: (results) {
-              if (results.isNotEmpty) {
-                setState(() {
-                  notebookId = results.first;
-
-                  // check if this notebook has at least one note
-                  if (notebooks
-                      .firstWhere((notebook) => notebook.id == notebookId)
-                      .notes
-                      .isEmpty) {
-                    EasyLoading.showError(
-                        "This notebook has no notes, please select another one or create a note.");
-
-                    // ? resets the notebookId to hide the pages dropdown again
+              items: notebooks
+                  .map((notebook) =>
+                      MultiSelectItem(notebook.id, notebook.subject))
+                  .toList(),
+              // selectedColor: Colors.blue,
+              listType: MultiSelectListType.CHIP,
+              decoration: BoxDecoration(
+                color: Colors.blue.withOpacity(0.1),
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+              ),
+              onSelectionChanged: (values) {
+                if (values.isEmpty) {
+                  setState(() {
                     notebookId = "";
-                  }
-                });
-              }
-            },
-            buttonIcon: const Icon(
-              Icons.book,
-              color: Colors.blue,
-            ),
-            buttonText: const Text(
-              "Notebooks",
-            ),
-          ),
-          notebookId.isNotEmpty
-              ? MultiSelectDialogField(
-                  title: const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Notebook Pages"),
-                      Text(
-                        "You can select multiple pages.",
-                        style: TextStyle(fontSize: 12),
-                      )
-                    ],
-                  ),
+                  });
+                }
 
-                  items: notebooks
-                      .firstWhere((notebook) => notebook.id == notebookId)
-                      .notes
-                      .map((note) => MultiSelectItem(note.id, note.title))
-                      .toList(),
-                  // selectedColor: Colors.blue,
-                  listType: MultiSelectListType.CHIP,
-                  decoration: BoxDecoration(
-                    color: Colors.blue.withOpacity(0.1),
-                    borderRadius: const BorderRadius.all(Radius.circular(8)),
-                  ),
-                  onConfirm: (results) async {
-                    setState(() {
-                      pages = results;
-                    });
-                  },
-                  buttonIcon: const Icon(
-                    Icons.pages,
-                    color: Colors.blue,
-                  ),
-                  buttonText: const Text(
-                    "Notebook Pages",
-                  ),
-                )
-              : const SizedBox(
-                  height: 10,
-                )
-        ],
+                if (values.length > 1) {
+                  // remove the first one
+                  values.removeAt(0);
+                }
+              },
+              onConfirm: (results) {
+                if (results.isNotEmpty) {
+                  setState(() {
+                    notebookId = results.first;
+
+                    // check if this notebook has at least one note
+                    if (notebooks
+                        .firstWhere((notebook) => notebook.id == notebookId)
+                        .notes
+                        .isEmpty) {
+                      EasyLoading.showError(
+                          "This notebook has no notes, please select another one or create a note.");
+
+                      // ? resets the notebookId to hide the pages dropdown again
+                      notebookId = "";
+                    }
+                  });
+                }
+              },
+              buttonIcon: const Icon(
+                Icons.book,
+                color: Colors.blue,
+              ),
+              buttonText: const Text(
+                "Notebooks",
+              ),
+            ),
+            notebookId.isNotEmpty
+                ? MultiSelectDialogField(
+                    title: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Notebook Pages"),
+                        Text(
+                          "You can select multiple pages.",
+                          style: TextStyle(fontSize: 12),
+                        )
+                      ],
+                    ),
+
+                    items: notebooks
+                        .firstWhere((notebook) => notebook.id == notebookId)
+                        .notes
+                        .map((note) => MultiSelectItem(note.id, note.title))
+                        .toList(),
+                    // selectedColor: Colors.blue,
+                    listType: MultiSelectListType.CHIP,
+                    decoration: BoxDecoration(
+                      color: Colors.blue.withOpacity(0.1),
+                      borderRadius: const BorderRadius.all(Radius.circular(8)),
+                    ),
+                    onConfirm: (results) async {
+                      setState(() {
+                        pages = results;
+                      });
+                    },
+                    buttonIcon: const Icon(
+                      Icons.pages,
+                      color: Colors.blue,
+                    ),
+                    buttonText: const Text(
+                      "Notebook Pages",
+                    ),
+                  )
+                : const SizedBox(
+                    height: 10,
+                  )
+          ],
+        ),
       ),
     );
   }

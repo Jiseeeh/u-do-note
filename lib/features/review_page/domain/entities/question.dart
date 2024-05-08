87a6cfe3e40b153997639a2c521b0a439b356dbd
Jiseeeh <johncarlo.camara1@gmail.com>
2024-05-08 19:00:48 +0800

    feat: add id

--- lib/features/review_page/domain/entities/question.dart
+++ lib/features/review_page/domain/entities/question.dart
@@ -1,9 +1,11 @@
 class QuestionEntity {
+  final String? id;
   final String question;
   final List<String> choices;
   final int correctAnswerIndex;
 
   QuestionEntity({
+    this.id,
     required this.question,
     required this.choices,
     required this.correctAnswerIndex,

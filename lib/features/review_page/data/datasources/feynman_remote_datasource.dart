4656ceef5a9dcefa18876d1fbb5bc4c1e34a71cd
Jiseeeh <johncarlo.camara1@gmail.com>
2024-05-08 18:57:57 +0800

    feat: add saving of session
    also include the the generation of quiz questions (sorry atomic commits)

--- lib/features/review_page/data/datasources/feynman_remote_datasource.dart
+++ lib/features/review_page/data/datasources/feynman_remote_datasource.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dart_openai/dart_openai.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -5,6 +7,7 @@ import 'package:u_do_note/core/firestore_collection_enum.dart';
 
 import 'package:u_do_note/core/logger/logger.dart';
 import 'package:u_do_note/features/review_page/data/models/feynman.dart';
+import 'package:u_do_note/features/review_page/data/models/question.dart';
 
 class FeynmanRemoteDataSource {
   final FirebaseFirestore _firestore;
@@ -70,10 +73,29 @@ class FeynmanRemoteDataSource {
     return chatCompletion.choices.first.message.content!.first.text!;
   }
 
-  Future<void> saveSession(FeynmanModel feynmanModel, String notebookId) async {
+  Future<String> saveSession(
+      FeynmanModel feynmanModel, String notebookId, String? docId) async {
     var userId = _auth.currentUser!.uid;
 
-    await _firestore
+    if (docId != null) {
+      await _firestore
+          .collection(FirestoreCollection.users.name)
+          .doc(userId)
+          .collection(FirestoreCollection.user_notes.name)
+          .doc(notebookId)
+          .collection(FirestoreCollection.remarks.name)
+          .doc(docId)
+          .update({
+        'messages':
+            feynmanModel.messages.map((message) => message.toJson()).toList(),
+        'recent_robot_messages': feynmanModel.recentRobotMessages,
+        'recent_user_messages': feynmanModel.recentUserMessages,
+      });
+
+      return docId;
+    }
+
+    var doc = await _firestore
         .collection(FirestoreCollection.users.name)
         .doc(userId)
         .collection(FirestoreCollection.user_notes.name)
@@ -88,9 +110,10 @@ class FeynmanRemoteDataSource {
       'recent_robot_messages': feynmanModel.recentRobotMessages,
       'recent_user_messages': feynmanModel.recentUserMessages,
       'score': '',
-      'total_items': '',
       'remark': '',
     });
+
+    return doc.id;
   }
 
   Future<List<FeynmanModel>> getOldSessions(String notebookId) async {
@@ -108,5 +131,210 @@ class FeynmanRemoteDataSource {
     return res.docs
         .map((model) => FeynmanModel.fromFirestore(model.id, model.data()))
         .toList();
+  }
+
+  Future<List<QuestionModel>> generateQuizQuestions(String content) async {
+    final systemMessage = OpenAIChatCompletionChoiceMessageModel(
+      content: [
+        OpenAIChatCompletionChoiceMessageContentItemModel.text(
+          """
+          As a helpful assistant, you will create a 10 question quiz based on the content that the user will give.
+          The response must be an array of json called "questions" with the properties "question", "choices" as an array of choice, and "correctAnswerIndex" as the index of the correct answer.
+          """,
+        ),
+      ],
+      role: OpenAIChatMessageRole.system,
+    );
+
+    final assistantMessage = OpenAIChatCompletionChoiceMessageModel(
+      content: [
+        OpenAIChatCompletionChoiceMessageContentItemModel.text(
+          """
+          {
+            "questions": [
+              {
+                "question": "What is the capital of France?",
+                "choices": ["Paris", "London", "Berlin", "Madrid"],
+                "correctAnswerIndex": 0
+              },
+              {
+                "question": "What is the largest planet in our solar system?",
+                "choices": ["Earth", "Mars", "Jupiter", "Saturn"],
+                "correctAnswerIndex": 2
+              },
+              {
+                "question": "What is the largest mammal?",
+                "choices": ["Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+                "correctAnswerIndex": 1
+              },
+              {
+                "question": "What is the largest ocean?",
+                "choices": ["Atlantic", "Indian", "Arctic", "Pacific"],
+                "correctAnswerIndex": 3
+              },
+              {
+                "question": "What is the largest country by land area?",
+                "choices": ["Russia", "Canada", "China", "United States"],
+                "correctAnswerIndex": 0
+              },
+              {
+                "question": "What is the largest desert?",
+                "choices": ["Sahara", "Arabian", "Gobi", "Kalahari"],
+                "correctAnswerIndex": 0
+              },
+              {
+                "question": "What is the largest mountain?",
+                "choices": ["Mount Everest", "K2", "Kangchenjunga", "Lhotse"],
+                "correctAnswerIndex": 0
+              },
+              {
+                "question": "What is the largest lake?",
+                "choices": ["Caspian Sea", "Superior", "Victoria", "Huron"],
+                "correctAnswerIndex": 0
+              },
+              {
+                "question": "What is the largest island?",
+                "choices": ["Greenland", "New Guinea", "Borneo", "Madagascar"],
+                "correctAnswerIndex": 0
+              },
+              {
+                "question": "What is the largest forest?",
+                "choices": ["Amazon", "Congo", "Daintree", "Taiga"],
+                "correctAnswerIndex": 0
+              }
+            ]
+          }
+          """,
+        ),
+      ],
+      role: OpenAIChatMessageRole.assistant,
+    );
+
+    final userMessage = OpenAIChatCompletionChoiceMessageModel(
+      content: [
+        OpenAIChatCompletionChoiceMessageContentItemModel.text(
+          """
+          Given the content: "$content", create a 10 question quiz.
+          """,
+        ),
+      ],
+      role: OpenAIChatMessageRole.user,
+    );
+
+    final requestMessages = [
+      systemMessage,
+      assistantMessage,
+      userMessage,
+    ];
+
+    OpenAIChatCompletionModel chatCompletion =
+        await OpenAI.instance.chat.create(
+      model: "gpt-3.5-turbo-0125",
+      responseFormat: {"type": "json_object"},
+      messages: requestMessages,
+      temperature: 0.2,
+      maxTokens: 600,
+    );
+
+    String? completionContent =
+        chatCompletion.choices.first.message.content!.first.text;
+
+    logger.i('content: $completionContent');
+    logger.i('finish reason: ${chatCompletion.choices.first.finishReason}');
+    logger.i(chatCompletion.systemFingerprint);
+    logger.i(chatCompletion.usage.promptTokens);
+
+    var decodedJson = json.decode(completionContent!);
+
+    List<QuestionModel> questions = [];
+
+    for (var question in decodedJson['questions']) {
+      questions.add(QuestionModel.fromJson(question));
+    }
+
+    return questions;
+  }
+
+  Future<void> saveQuizResults(
+      FeynmanModel feynmanModel, String notebookId) async {
+    var userId = _auth.currentUser!.uid;
+
+    final systemMessage = OpenAIChatCompletionChoiceMessageModel(
+      content: [
+        OpenAIChatCompletionChoiceMessageContentItemModel.text(
+          """
+          As a helpful assistant, you will analyze the performance of the student in the quiz.
+          Return the result as a json with the property "remark" It could be "Excellent", "Good", "Average", "Poor", or "Very Poor".
+          """,
+        ),
+      ],
+      role: OpenAIChatMessageRole.system,
+    );
+
+    final assistantMessage = OpenAIChatCompletionChoiceMessageModel(
+      content: [
+        OpenAIChatCompletionChoiceMessageContentItemModel.text(
+          """
+          {
+            "remark": "Good"
+          }
+          """,
+        ),
+      ],
+      role: OpenAIChatMessageRole.assistant,
+    );
+
+    final userMessage = OpenAIChatCompletionChoiceMessageModel(
+      content: [
+        OpenAIChatCompletionChoiceMessageContentItemModel.text(
+          """
+          Given the score of the student: ${feynmanModel.score}, analyze the performance of the student in the quiz and give a remark.
+          """,
+        ),
+      ],
+      role: OpenAIChatMessageRole.user,
+    );
+
+    final requestMessages = [
+      systemMessage,
+      assistantMessage,
+      userMessage,
+    ];
+
+    OpenAIChatCompletionModel chatCompletion =
+        await OpenAI.instance.chat.create(
+      model: "gpt-3.5-turbo-0125",
+      responseFormat: {"type": "json_object"},
+      messages: requestMessages,
+      temperature: 0.2,
+      maxTokens: 600,
+    );
+
+    String? completionContent =
+        chatCompletion.choices.first.message.content!.first.text;
+
+    logger.i('content: $completionContent');
+    logger.i('finish reason: ${chatCompletion.choices.first.finishReason}');
+    logger.i(chatCompletion.systemFingerprint);
+    logger.i(chatCompletion.usage.promptTokens);
+
+    var decodedJson = json.decode(completionContent!);
+
+    var remark = decodedJson['remark'];
+
+    await _firestore
+        .collection(FirestoreCollection.users.name)
+        .doc(userId)
+        .collection(FirestoreCollection.user_notes.name)
+        .doc(notebookId)
+        .collection(FirestoreCollection.remarks.name)
+        .doc(feynmanModel.id)
+        .update({
+      'questions':
+          feynmanModel.questions!.map((question) => question.toJson()).toList(),
+      'selected_answers_index': feynmanModel.selectedAnswersIndex,
+      'score': feynmanModel.score,
+      'remark': remark,
+    });
   }
 }

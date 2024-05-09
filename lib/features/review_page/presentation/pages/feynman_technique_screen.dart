c4179934ef3e36f538bafcbc750d682380f355b0
Jiseeeh <johncarlo.camara1@gmail.com>
2024-05-09 15:57:21 +0800

    feat: add dialog for choosing new session name

--- lib/features/review_page/presentation/pages/feynman_technique_screen.dart
+++ lib/features/review_page/presentation/pages/feynman_technique_screen.dart
@@ -34,6 +34,8 @@ class _FeynmanTechniqueScreenState
   final List<String> recentRobotMessages = [];
   final List<String> recentUserMessages = [];
   var isRobotThinking = false;
+  var formKey = GlobalKey<FormState>();
+  var newSessionNameController = TextEditingController();
   String? docId; // ? to track if the user is re-saving the session
   FeynmanModel? feynmanModel;
 
@@ -50,6 +52,8 @@ class _FeynmanTechniqueScreenState
     super.initState();
 
     if (widget.feynmanEntity != null) {
+      docId = widget.feynmanEntity!.id;
+
       _initOldSession();
       return;
     }
@@ -97,13 +101,59 @@ class _FeynmanTechniqueScreenState
     FocusScope.of(context).requestFocus(FocusNode());
 
     if (message.text.toLowerCase() == "quiz") {
+      if (widget.feynmanEntity != null) {
+        var willTakeNewQuiz = await showDialog(
+            barrierDismissible: false,
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text('Quiz'),
+                content: const Text(
+                    'Do you want to check the results of the previous quiz in this session or start a new quiz?'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(false);
+                    },
+                    child: const Text('Check old results'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(true);
+                    },
+                    child: const Text('Start a quiz'),
+                  ),
+                ],
+              );
+            });
+
+        if (!context.mounted) return;
+
+        if (!willTakeNewQuiz) {
+          context.router.push(QuizResultsRoute(
+              questions: widget.feynmanEntity!.questions!,
+              correctAnswersIndex: widget.feynmanEntity!.questions!
+                  .map((question) => question.correctAnswerIndex)
+                  .toList(),
+              selectedAnswersIndex:
+                  widget.feynmanEntity!.selectedAnswersIndex!));
+          return;
+        }
+      }
+
+      if (!context.mounted) return;
+
       var willTakeQuiz = await showDialog(
+          barrierDismissible: false,
           context: context,
           builder: (context) {
             return AlertDialog(
               title: const Text('Quiz'),
-              content: const Text(
-                  'Are you ready to take a quiz? You can take a quiz after you finish the chat.'),
+              content: widget.feynmanEntity == null
+                  ? const Text(
+                      'Are you ready to take a quiz? You can take a quiz after you finish the chat.')
+                  : const Text(
+                      'Starting a new quiz will make a new session. Do you want to proceed?'),
               actions: [
                 TextButton(
                   onPressed: () {
@@ -122,6 +172,11 @@ class _FeynmanTechniqueScreenState
           });
 
       if (!willTakeQuiz || !context.mounted) return;
+
+      if (widget.feynmanEntity!.id == null && feynmanModel?.id == null) {
+        EasyLoading.showInfo("Please save the session first.");
+        return;
+      }
 
       EasyLoading.show(
           status: 'Generating quiz...',
@@ -142,8 +197,63 @@ class _FeynmanTechniqueScreenState
 
       if (!context.mounted) return;
 
-      if (feynmanModel?.id == null) {
-        EasyLoading.showInfo("Please save the session first.");
+      if (widget.feynmanEntity != null) {
+        var newSessionName = await showDialog(
+            barrierDismissible: false,
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text('Quiz'),
+                content: Form(
+                  key: formKey,
+                  child: TextFormField(
+                    controller: newSessionNameController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please enter a session name.";
+                      }
+
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: "Session Name",
+                      hintText: 'planets-retake',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(null);
+                    },
+                    child: const Text('Cancel'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      if (!formKey.currentState!.validate()) {
+                        return;
+                      }
+
+                      Navigator.of(context).pop(newSessionNameController.text);
+                    },
+                    child: const Text('Continue'),
+                  ),
+                ],
+              );
+            });
+
+        if (newSessionName == null) return;
+        if (!context.mounted) return;
+
+        var feynmanModel = FeynmanModel.fromEntity(widget.feynmanEntity!)
+            .copyWith(
+                questions: quizQuestions,
+                recentRobotMessages: recentRobotMessages,
+                recentUserMessages: recentUserMessages);
+
+        context.router.push(QuizRoute(
+            feynmanModel: feynmanModel, newSessionName: newSessionName));
         return;
       }
 

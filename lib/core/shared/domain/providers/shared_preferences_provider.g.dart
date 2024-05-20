45e93545279082b91c68ce3792efe70c8162b69f
Jiseeeh <johncarlo.camara1@gmail.com>
2024-05-20 15:30:53 +0800

    feat: add shared prefs provider

---
+++ lib/core/shared/domain/providers/shared_preferences_provider.g.dart
@@ -0,0 +1,40 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'shared_preferences_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$sharedPreferencesHash() => r'87f7c0811db991852c74d72376df550977c6d6db';
+
+/// See also [sharedPreferences].
+@ProviderFor(sharedPreferences)
+final sharedPreferencesProvider = FutureProvider<SharedPreferences>.internal(
+  sharedPreferences,
+  name: r'sharedPreferencesProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$sharedPreferencesHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef SharedPreferencesRef = FutureProviderRef<SharedPreferences>;
+String _$sharedPrefsHash() => r'7ddd269b41fe42c6213c142ff7ca20830d1354ef';
+
+/// See also [SharedPrefs].
+@ProviderFor(SharedPrefs)
+final sharedPrefsProvider =
+    AutoDisposeNotifierProvider<SharedPrefs, void>.internal(
+  SharedPrefs.new,
+  name: r'sharedPrefsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$sharedPrefsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SharedPrefs = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member

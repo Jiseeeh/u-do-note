90384d20813ccc0682552dc2dc9d88c74d26b8a6
Jiseeeh <johncarlo.camara1@gmail.com>
2024-05-24 17:16:06 +0800

    chore: remove unused

--- lib/core/shared/theme/app_theme.dart
+++ lib/core/shared/theme/app_theme.dart
@@ -1,39 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:u_do_note/core/shared/data/local/storage_service.dart';
-import 'package:u_do_note/core/shared/domain/providers/shared_preferences_storage_service_provider.dart';
 import 'package:u_do_note/core/shared/theme/colors.dart';
 import 'package:u_do_note/core/shared/theme/text_styles.dart';
 import 'package:u_do_note/core/shared/theme/text_theme.dart';
-
-final appThemeProvider = StateNotifierProvider<AppThemeModeNotifier, ThemeMode>(
-  (ref) {
-    final storage = ref.watch(storageServiceProvider);
-    return AppThemeModeNotifier(storage);
-  },
-);
-
-class AppThemeModeNotifier extends StateNotifier<ThemeMode> {
-  final StorageService storageService;
-
-  ThemeMode currentTheme = ThemeMode.light;
-
-  AppThemeModeNotifier(this.storageService) : super(ThemeMode.light) {
-    getCurrentTheme();
-  }
-
-  void toggleTheme() {
-    state = state == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
-    storageService.set('AppTheme', state.name);
-  }
-
-  void getCurrentTheme() async {
-    final theme = await storageService.get('AppTheme');
-    final value = ThemeMode.values.byName('${theme ?? 'light'}');
-    state = value;
-  }
-}
 
 class AppTheme {
   static ThemeData get darkTheme {

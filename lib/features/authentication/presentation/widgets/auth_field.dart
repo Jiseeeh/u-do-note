c8128c1b6a7b77ae8f3438979650cf6272803813
Jiseeeh <johncarlo.camara1@gmail.com>
2024-05-24 17:48:06 +0800

    chore: remove dup import

--- lib/features/authentication/presentation/widgets/auth_field.dart
+++ lib/features/authentication/presentation/widgets/auth_field.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:u_do_note/core/shared/theme/colors.dart';
-import 'package:u_do_note/core/shared/theme/text_styles.dart';
 
 import 'package:u_do_note/core/shared/theme/colors.dart';
 import 'package:u_do_note/core/shared/theme/text_styles.dart';

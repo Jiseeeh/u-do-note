2782a278aa9152c712986a3c59359d6cce955bfc
Jiseeeh <johncarlo.camara1@gmail.com>
2024-05-31 23:50:44 +0800

    feat: add app state

---
+++ lib/core/shared/presentation/providers/app_state_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_state_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appStateHash() => r'b612972c4c407e0f3669bcb2ef1fce6ab3f0f9da';
+
+/// See also [AppState].
+@ProviderFor(AppState)
+final appStateProvider = NotifierProvider<AppState, AppStateEntity>.internal(
+  AppState.new,
+  name: r'appStateProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$appStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AppState = Notifier<AppStateEntity>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member

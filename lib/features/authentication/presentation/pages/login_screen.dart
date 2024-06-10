1de76b210d920754da5b6c1516c43fe51928e888
Jiseeeh <johncarlo.camara1@gmail.com>
2024-06-11 01:38:22 +0800

    remove unused import

--- lib/features/authentication/presentation/pages/login_screen.dart
+++ lib/features/authentication/presentation/pages/login_screen.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 import '../widgets/social_icon.dart';

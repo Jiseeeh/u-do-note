fba53864b4ba3b1c76bbd63924029325ce98489d
Jiseeeh <johncarlo.camara1@gmail.com>
2024-07-12 23:11:21 +0800

    feat: wrap app with easy_localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dart_openai/dart_openai.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
@@ -6,6 +7,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 import 'firebase_options.dart';
+import 'package:u_do_note/core/constant.dart' as constants;
 import 'package:u_do_note/core/shared/domain/providers/app_theme_provider.dart';
 import 'package:u_do_note/core/shared/theme/app_theme.dart';
 import 'package:u_do_note/observers.dart';
@@ -14,13 +16,19 @@ import 'package:u_do_note/env/env.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
 
   await initDeps();
 
   runApp(
-    ProviderScope(
-      observers: [Observers()],
-      child: MainApp(),
+    EasyLocalization(
+      supportedLocales: constants.supportedLocales,
+      startLocale: constants.defaultLocale,
+      path: 'assets/translations',
+      child: ProviderScope(
+        observers: [Observers()],
+        child: MainApp(),
+      ),
     ),
   );
 }
@@ -43,6 +51,9 @@ class MainApp extends ConsumerWidget {
     return ResponsiveSizer(
         builder: (context, orientation, screenType) => MaterialApp.router(
               title: 'U Do Note',
+              localizationsDelegates: context.localizationDelegates,
+              supportedLocales: context.supportedLocales,
+              locale: context.locale,
               theme: AppTheme.lightTheme,
               darkTheme: AppTheme.darkTheme,
               themeMode: themeMode,

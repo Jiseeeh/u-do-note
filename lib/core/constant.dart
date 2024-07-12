1ac028c6c6b6aafc81d75f0fb77bf7604a426f49
Jiseeeh <johncarlo.camara1@gmail.com>
2024-07-12 23:11:32 +0800

    feat: add app constants

---
+++ lib/core/constant.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Locale defaultLocale = Locale('en');
+const Locale filLocale = Locale('fil');
+const List<Locale> supportedLocales = [defaultLocale, filLocale];

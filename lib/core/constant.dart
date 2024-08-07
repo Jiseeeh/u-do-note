5ac5d05144a7b1879c25f1a8c37202f210a108ea
Jiseeeh <johncarlo.camara1@gmail.com>
2024-08-07 17:09:32 +0800

    feat: add constant variables

--- lib/core/constant.dart
+++ lib/core/constant.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'package:u_do_note/features/review_page/data/models/feynman.dart';
+import 'package:u_do_note/features/review_page/data/models/leitner.dart';
+import 'package:u_do_note/features/review_page/data/models/pomodoro.dart';
+
+/* ==================== Localizations ==================== */
 const Locale defaultLocale = Locale('en');
 const Locale filLocale = Locale('fil');
 const List<Locale> supportedLocales = [defaultLocale, filLocale];
+
+/* ==================== Notebook ========================= */
+const defaultTechniquesUsage = {
+  LeitnerSystemModel.name: 0,
+  FeynmanModel.name: 0,
+  PomodoroModel.name: 0
+};

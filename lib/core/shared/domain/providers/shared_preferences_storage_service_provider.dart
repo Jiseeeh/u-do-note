af40bd8089ac3ff26e5fd47caf739321b0641626
Jiseeeh <johncarlo.camara1@gmail.com>
2024-08-15 22:49:13 +0800

    feat: delete file

--- lib/core/shared/domain/providers/shared_preferences_storage_service_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'package:u_do_note/core/shared/data/local/shared_prefs_storage_service.dart';
-
-final storageServiceProvider = Provider((ref) {
-  final SharedPrefsService prefsService = SharedPrefsService();
-  prefsService.init();
-  return prefsService;
-});

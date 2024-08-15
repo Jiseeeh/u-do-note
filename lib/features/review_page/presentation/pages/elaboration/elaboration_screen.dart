6566826f65fb0b350c4dcf0ebdbb2f1fc6e50c37
Jiseeeh <johncarlo.camara1@gmail.com>
2024-08-15 22:48:28 +0800

    feat: move directory

--- lib/features/review_page/presentation/pages/elaboration/elaboration_screen.dart
+++ lib/features/review_page/presentation/pages/elaboration/elaboration_screen.dart
@@ -1,13 +1,17 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:u_do_note/core/error/failures.dart';
 import 'package:u_do_note/core/logger/logger.dart';
+import 'package:u_do_note/core/shared/presentation/providers/shared_provider.dart';
 import 'package:u_do_note/features/review_page/data/models/elaboration.dart';
 import 'package:u_do_note/features/review_page/presentation/providers/elaboration_provider.dart';
 import 'package:u_do_note/features/review_page/presentation/providers/review_screen_provider.dart';
+import 'package:u_do_note/routes/app_route.dart';
 
 @RoutePage()
 class ElaborationScreen extends ConsumerWidget {
@@ -72,9 +76,32 @@ class ElaborationScreen extends ConsumerWidget {
                       child: const Text('No'),
                     ),
                     TextButton(
-                      onPressed: () {
-                        // TODO: Start a quiz
-                        Navigator.of(dialogContext).pop();
+                      onPressed: () async {
+                        var sharedRepository =
+                            ref.read(sharedProvider.notifier);
+
+                        EasyLoading.show(
+                            status: context.tr("generate_quiz"),
+                            maskType: EasyLoadingMaskType.black,
+                            dismissOnTap: false);
+
+                        var res = await sharedRepository.generateQuizQuestions(
+                            content: content);
+
+                        EasyLoading.dismiss();
+
+                        if (!dialogContext.mounted) return;
+
+                        if (res is Failure) {
+                          EasyLoading.showError(context.tr("generate_quiz_e"));
+                          logger.e('Failed to generate quiz questions: $res');
+
+                          Navigator.of(dialogContext).pop();
+                        } else {
+                          context.router.replace(ElaborationQuizRoute(
+                            questions: res,
+                          ));
+                        }
                       },
                       child: const Text('Yes'),
                     ),

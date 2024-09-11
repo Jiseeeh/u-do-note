ef0c76d9853049701f0b1add2fb10dd7c2f28e20
Marc Benedict Santos <s.marcbenedict@gmail.com>
2024-09-11 21:46:10 +0800

    fix: added temporary color for splash screen background

--- lib/features/authentication/presentation/pages/splash_screen.dart
+++ lib/features/authentication/presentation/pages/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lottie/lottie.dart';
+import 'package:u_do_note/core/shared/theme/colors.dart';
 
 import 'package:u_do_note/routes/app_route.dart';
 
@@ -32,7 +33,7 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      backgroundColor: AppColors.primaryNew,
       body: Center(
         child: Lottie.asset('assets/images/logo/splash.json'),
       ),

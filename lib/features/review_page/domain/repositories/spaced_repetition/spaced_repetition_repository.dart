5e1684f3806e5449c3c479ea7529324b085d345f
Jiseeeh <johncarlo.camara1@gmail.com>
2024-10-17 15:33:36 +0800

    feat: remove generate content

--- lib/features/review_page/domain/repositories/spaced_repetition/spaced_repetition_repository.dart
+++ lib/features/review_page/domain/repositories/spaced_repetition/spaced_repetition_repository.dart
@@ -1,13 +1,9 @@
 import 'package:dartz/dartz.dart';
 
-import 'package:u_do_note/core/enums/assistance_type.dart';
 import 'package:u_do_note/core/error/failures.dart';
 import 'package:u_do_note/features/review_page/data/models/spaced_repetition.dart';
 
 abstract class SpacedRepetitionRepository {
-  Future<Either<Failure, String>> generateContent(
-      String content, AssistanceType type);
-
   Future<Either<Failure, String>> saveQuizResults(
       SpacedRepetitionModel spacedRepetitionModel);
 }

47fbcea9ee0d94d4d36f0ece9eb97b9f9c511d90
Jiseeeh <johncarlo.camara1@gmail.com>
2024-10-17 22:01:42 +0800

    feat: disable limit temporarily

--- lib/features/note_taking/presentation/pages/notebook_pages_screen.dart
+++ lib/features/note_taking/presentation/pages/notebook_pages_screen.dart
@@ -153,11 +153,11 @@ class _NotebookPagesScreenState extends ConsumerState<NotebookPagesScreen> {
                 if (result != null) {
                   var first = result.files.first;
 
-                  if (first.size > 4 * 1024 * 1024) {
-                    EasyLoading.showError(context.tr("file_size_e"),
-                        duration: const Duration(seconds: 2));
-                    return;
-                  }
+                  // if (first.size > 4 * 1024 * 1024) {
+                  //   EasyLoading.showError(context.tr("file_size_e"),
+                  //       duration: const Duration(seconds: 2));
+                  //   return;
+                  // }
 
                   if (!extensions.contains(first.extension)) {
                     EasyLoading.showError(context.tr("allowed_files"),

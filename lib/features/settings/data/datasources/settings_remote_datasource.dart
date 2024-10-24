6fbc67d63075bb4386266be84af497fbbef61553
Jiseeeh <johncarlo.camara1@gmail.com>
2024-10-24 18:29:02 +0800

    fix: disconnect google sign in on available devices only

--- lib/features/settings/data/datasources/settings_remote_datasource.dart
+++ lib/features/settings/data/datasources/settings_remote_datasource.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:google_api_availability/google_api_availability.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -22,11 +23,18 @@ class SettingsRemoteDataSource {
     logger.d("User $userId is signing out.");
 
     try {
-      GoogleSignIn googleSignIn = GoogleSignIn();
-      await googleSignIn.disconnect();
-      logger.d("google sign in disconnected");
+      GooglePlayServicesAvailability availability = await GoogleApiAvailability
+          .instance
+          .checkGooglePlayServicesAvailability();
+
+      if (availability == GooglePlayServicesAvailability.success) {
+        GoogleSignIn googleSignIn = GoogleSignIn();
+        await googleSignIn.disconnect();
+
+        logger.d("google sign in disconnected");
+      }
     } catch (error) {
-      logger.d("Could not disconnect google sign in with error: $error");
+      logger.d("could not disconnect google sign in with error: $error");
     }
   }
 

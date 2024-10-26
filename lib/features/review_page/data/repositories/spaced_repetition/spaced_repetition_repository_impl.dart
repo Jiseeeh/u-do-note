3c1e2e6131df330939323655cedb1b18f8c0d0f3
Jiseeeh <johncarlo.camara1@gmail.com>
2024-10-26 20:46:57 +0800

    chore: remove invalid override

--- lib/features/review_page/data/repositories/spaced_repetition/spaced_repetition_repository_impl.dart
+++ lib/features/review_page/data/repositories/spaced_repetition/spaced_repetition_repository_impl.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
 
-import 'package:u_do_note/core/enums/assistance_type.dart';
 import 'package:u_do_note/core/error/failures.dart';
 import 'package:u_do_note/features/review_page/data/datasources/spaced_repetition/spaced_repetition_remote_datasource.dart';
 import 'package:u_do_note/features/review_page/data/models/spaced_repetition.dart';
@@ -10,19 +9,6 @@ class SpacedRepetitionImpl implements SpacedRepetitionRepository {
   final SpacedRepetitionRemoteDataSource _spacedRepetitionRemoteDataSource;
 
   const SpacedRepetitionImpl(this._spacedRepetitionRemoteDataSource);
-
-  @override
-  Future<Either<Failure, String>> generateContent(
-      String content, AssistanceType type) async {
-    try {
-      var res = await _spacedRepetitionRemoteDataSource.generateContent(
-          type, content);
-
-      return Right(res);
-    } catch (e) {
-      return Left(GenericFailure(message: e.toString()));
-    }
-  }
 
   @override
   Future<Either<Failure, String>> saveQuizResults(

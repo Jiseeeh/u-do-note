8c195b9c0167ab45aeb322f61ffa8434e62b80c5
Jiseeeh <johncarlo.camara1@gmail.com>
2024-11-03 23:31:40 +0800

    feat: add missing learning methods

--- lib/core/constant.dart
+++ lib/core/constant.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:u_do_note/features/review_page/data/models/acronym.dart';
+import 'package:u_do_note/features/review_page/data/models/active_recall.dart';
+import 'package:u_do_note/features/review_page/data/models/blurting.dart';
 import 'package:u_do_note/features/review_page/data/models/elaboration.dart';
 
 import 'package:u_do_note/features/review_page/data/models/feynman.dart';
 import 'package:u_do_note/features/review_page/data/models/leitner.dart';
 import 'package:u_do_note/features/review_page/data/models/pomodoro.dart';
+import 'package:u_do_note/features/review_page/data/models/pq4r.dart';
+import 'package:u_do_note/features/review_page/data/models/spaced_repetition.dart';
+import 'package:u_do_note/features/review_page/data/models/sq3r.dart';
 
 /* ==================== Localizations ==================== */
 const Locale defaultLocale = Locale('en');
@@ -16,6 +22,12 @@ const defaultTechniquesUsage = {
   FeynmanModel.name: 0,
   PomodoroModel.name: 0,
   ElaborationModel.name: 0,
+  AcronymModel.name: 0,
+  BlurtingModel.name: 0,
+  SpacedRepetitionModel.name: 0,
+  ActiveRecallModel.name: 0,
+  Sq3rModel.name: 0,
+  Pq4rModel.name: 0,
 };
 
 /* ==================== Fields ========================= */
